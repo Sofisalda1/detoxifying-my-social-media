7fda6bb7ff0b0dcabe828b2d7aa6b0b23468f73d
katwegner <kwegner2102@gmail.com>
2022-04-28 22:28:14 +0200

    fixed errors in preprocessing.py

--- modeling/preprocessing.py
+++ modeling/preprocessing.py
@@ -10,7 +10,8 @@ import numpy as np
 import warnings
 warnings.filterwarnings('ignore')
 from toolbox import *
-
+WNlemma = nltk.WordNetLemmatizer()
+stemmer = nltk.PorterStemmer()
 import mlflow
 
 
@@ -19,14 +20,15 @@ from config import TRACKING_URI, EXPERIMENT_NAME
 mlflow.set_tracking_uri(TRACKING_URI)
 mlflow.set_experiment(EXPERIMENT_NAME)
 
+
 #%%
 def __get_data():
     DATA_NAME = 'train_wikipedia_pre_clean'
     train = pd.read_csv("./data/" + DATA_NAME + ".csv")
 
     # cleaning data and preparing
-    Y = train["toxic"][:10000]
-    X = train["comment_text"][:10000]
+    Y = train["toxic"]
+    X = train["comment_text"]
     return X,Y, DATA_NAME
 
 
@@ -36,16 +38,12 @@ def __get_data():
 
 min_df = 20 # 1 20
 stop_words = 'english' # {} 'english'
-
-WNlemma = nltk.WordNetLemmatizer()
-stemmer = nltk.PorterStemmer()
-
-
 ngram = 1 # 1, 2
-lowercase = False # True, False
+lowercase = True # True, False
 tokenizer = tweettokenizer
 vectorizer = CountVectorizer # TfidfVectorizer CountVectorizer
-word_generalization = WNlemma.lemmatize # stemmer.stem WNlemma.lemmatize
+word_generalization = stemmer.stem # stemmer.stem WNlemma.lemmati
+model = LogisticRegression()
 
 #%%
 #########################################
@@ -54,28 +52,18 @@ word_generalization = WNlemma.lemmatize # stemmer.stem WNlemma.lemmatize
 
 
 def run_preprocessing():
-    min_df = 20 # 1 20
-    stop_words = 'english' # {} 'english'
-    WNlemma = nltk.WordNetLemmatizer()
-    stemmer = nltk.PorterStemmer()
-    ngram = 1 # 1, 2
-    lowercase = False # True, False
-    #tokenizer = tweettokenizer
-    vectorizer = CountVectorizer # TfidfVectorizer CountVectorizer
-    word_generalization = WNlemma.lemmatize # stemmer.stem WNlemma.lemmati
-    model = LogisticRegression
     X, Y, DATA_NAME = __get_data()
     logger.info("Preprocessing Data")
     mlflow.set_tracking_uri(TRACKING_URI)
     mlflow.set_experiment(EXPERIMENT_NAME)
     with mlflow.start_run():
         mlflow.log_params({"Data": DATA_NAME})
-        mlflow.log_params({"min_df": min_df,"stop_words": stop_words,
-                        "ngram": ngram, "lowercase": lowercase, "tokenizer": tokenizer,
-                        "vectorizer": vectorizer, "word_generalization": word_generalization, "ml_model": model})
-        # preprocess
-        X_preproc = nlp_preprocess(X, vectorizer, word_generalization, min_df, stop_words, ngram, lowercase)
-        # fit models
+        mlflow.log_params({"Min Occurrence": min_df,"Stop Words": stop_words,
+                        "Ngram": ngram, "Lowercase": lowercase, "Tokenizer": tokenizer,
+                        "Vectorizer": vectorizer, "Word_Summary": word_generalization, "ml_model": model})
+
+        X_preproc, vect = nlp_preprocess(X, tokenizer,vectorizer, word_generalization, min_df, stop_words, ngram, lowercase)
+        mlflow.log_params({"Vocabulary": len(vect.vocabulary_)})
         Precision, ROC = fit_nlp(X_preproc, Y, model)
         mlflow.log_metric("Precision", Precision)
         mlflow.log_metric("ROC", ROC)
